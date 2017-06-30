2f9c3c1441fa7cc5e93c14ed92e2a0fa092c01a8
Justin Garavel <xxjeep@gmail.com>
2017-06-30 16:26:32 +0900

    prototyping first dashboard. Start with greeting based on time of day.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function MorningGreeting(props) {
+  return <h1>Good Morning, Justin.</h1>;
 }
 
-export default App;
+function AfternoonGreeting(props) {
+  return <h1>Good Afternoon, Justin.</h1>;
+}
+
+function Greeting(props) {
+  if (new Date().getHours() < 12) {
+    return <MorningGreeting />;
+  }
+  return <AfternoonGreeting />;
+}
+
+ReactDOM.render(
+  <Greeting />,
+  document.getElementById('root')
+);
