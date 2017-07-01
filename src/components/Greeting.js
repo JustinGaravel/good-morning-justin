550c46712475f0c605374d157d19d9d4634a7272
Justin Garavel <xxjeep@gmail.com>
2017-07-01 09:51:41 +0900

    morning loop not executing properly

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -7,9 +7,12 @@ export default class Greeting extends React.Component {
   }
 
   render() {
-    if (this.hour < 12) {
+    if (this.hour > 12) {
+      console.log('enter positive part of loop');
       return <h1>Good Morning, Justin.</h1>;
+    } else {
+      console.log('entered "else"');
+      return <h1>Good Afternoon, Justin.</h1>;
     }
-    return <h1>Good Afternoon, Justin.</h1>;
   }
 }
