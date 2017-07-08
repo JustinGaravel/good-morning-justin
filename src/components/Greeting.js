29795697c6441b42713e54ab6632aaab05c5cbe1
Justin Garavel <xxjeep@gmail.com>
2017-07-08 22:38:11 +0900

    cleaned up code and added a bonus phrase for the morning

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -7,11 +7,12 @@ export default class Greeting extends React.Component {
   }
 
   render() {
-    console.log(this.state.hour);
 
-    if (20 > this.state.hour > 12) {
+    if (this.state.hour < 8) {
+      return <h1>Great Early start! Good Morning, Justin.</h1>;
+    } else if (this.state.hour < 12) {
       return <h1>Good Morning, Justin.</h1>;
-    } else if (this.state.hour < 22) {
+    } else if (this.state.hour < 21) {
       return <h1>Good Afternoon, Justin.</h1>;
     } else {
       return <h1>Good Night, Justin.</h1>;
